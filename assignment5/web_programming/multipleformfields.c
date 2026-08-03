8204e3407b99c82245b525df69d684aa68bea080
Divya Gupta <2007gupta.divya@gmail.com>
2026-08-03 20:29:38 +0000

    templateliterals implementation

---
+++ assignment5/web_programming/multipleformfields.c
@@ -0,0 +1,12 @@
+const fields = [
+  "First Name",
+  "Last Name",
+  "Email"
+];
+
+function generateForm() {
+  return fields.map(field => `
+    <label>${field}</label>
+    <input type="text">
+  `).join("");
+}
